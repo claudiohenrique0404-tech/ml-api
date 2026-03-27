11545b74f3db56a8ba6c29034e3dbcc43418eabb
claudiohenrique0404-tech <claudiohenrique0404@icloud.com>
2026-03-27 23:13:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from sklearn.ensemble import RandomForestClassifier
 
 app = FastAPI()
 
-# modelo fake inicial (depois evoluímos)
 model = RandomForestClassifier()
 
 X = np.random.rand(100,10)
 y = np.random.randint(0,2,100)
 
 model.fit(X,y)
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
 
 @app.post("/predict")
 def predict(data: dict):
